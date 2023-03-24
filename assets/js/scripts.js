13e78231eb026bad363a9ce47cd9c43df657e64c
Villager Boy GitHub <103837860+VillagerBoyGitHub@users.noreply.github.com>
2023-03-24 08:58:47 +0100

    asas

---
+++ assets/js/scripts.js
@@ -0,0 +1,10 @@
+
+function copyIP() {
+    navigator.clipboard.writeText("dragon-pe.ddns.ne");
+    alert("Copied to clipboard!");
+}
+
+function copyPort() {
+    navigator.clipboard.writeText("7224");
+    alert("Copied to clipboard!");
+}
