784f66beaf2f395de9f39ac269e0f4bed271552f
Villager Boy GitHub <103837860+VillagerBoyGitHub@users.noreply.github.com>
2023-04-11 12:48:15 +0200

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,8 @@
 
 function copyIP() {
     navigator.clipboard.writeText("dragon-pe.ddns.ne");
-    alert("Copied to clipboard!");
 }
 
 function copyPort() {
     navigator.clipboard.writeText("7224");
-    alert("Copied to clipboard!");
 }
